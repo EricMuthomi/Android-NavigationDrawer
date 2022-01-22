117ab085cf9be7318c9f047e7c85457fa4232661
Eric Palmer <muthomiericpalmer@gmail.com>
2022-01-22 18:03:31 +0300

    navigation drawer,first commit

---
+++ app/src/main/java/com/ericpalmer/navigationdrawer/MainActivity.java
@@ -0,0 +1,111 @@
+package com.ericpalmer.navigationdrawer;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.ericpalmer.CartFragment;
+import com.ericpalmer.HomeFragment;
+import com.ericpalmer.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //declare our navigation view in order to set onclicks for our menu items
+        NavigationView navigationView = findViewById(R.id.drawer_navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Set our toolbar as the new actionbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //create the hamburger menu icon responsible for the opening of the navigation drawer when it is clicked
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState==null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new HomeFragment()).commit();
+        navigationView.setCheckedItem(R.id.menu_home);}
+
+    }
+    //override the onbackpressed method in order to not close the navigation drawer when we press the back btn
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+           getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new HomeFragment()).commit();
+                break;
+            case R.id.menu_cart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new CartFragment()).commit();
+
+                break;
+            case R.id.menu_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,new ProfileFragment()).commit();
+                break;
+            case R.id.menu_share:
+                Toast.makeText(MainActivity.this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu_send:
+                Toast.makeText(MainActivity.this, "Send", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return false;
+    }
+
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        MenuInflater menuInflater=getMenuInflater();
+//        menuInflater.inflate(R.menu.menu,menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//        switch (item.getItemId()){
+//            case R.id.menu_home:
+//                Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.menu_cart:
+//                Toast.makeText(MainActivity.this, "Cart", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.menu_categories:
+//                Toast.makeText(MainActivity.this, "Cartegories", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.menu_profile:
+//                Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
+}
